5b5af200813b75efbd2e9abc4a2546d02ed52bd6
GanishwarArun <ganishwarips@gmail.com>
2024-08-15 00:22:32 +0530

    git hub coomit

---
+++ script.js
@@ -0,0 +1,69 @@
+// console.log("hi ji")
+
+// let num = 2
+// let num1 = 4
+// let add = num + num1
+// console.log(add)
+
+// const add = (num1, num2) => num1 + num2
+// console.log(add(10, 20))
+ 
+// function add(num1, num2) {
+//     return num1 + num2
+
+// }
+// const result = add
+// console.log(result(98, 78))
+//     console.log (result ( 65, 76))
+
+
+// for in loop (array of object)
+
+// const obj = {
+//     name: "Ganishwar Arun",
+//     place: "Chennai",
+//     gender: "Male",
+//     company: "Arivazhagi Enterprises",
+// }
+//     for(let key in obj) {
+//         console.log(key)
+//         console.log (obj [key])
+//     }
+
+
+//array of object
+
+// const familyMembers = [
+//   { name: "Arun", relation: "youngest son" },
+//   { name: "Jagn", relation: "eldest son" },
+//   { name: "arivu", relation: "mother" },
+//   { name: "kumar", relation: "father" },
+  
+// ]
+
+// familyMembers.forEach((member) => {
+//   console.log (member.name, member.relation)
+// })
+
+
+// object.assign
+// const obj = { name: "Arun" };
+// const obj1 = Object.assign({}, obj);
+// console.log(obj, obj1);
+// obj1.name = "Ganishwar Arun";
+// console.log(obj, obj1);
+
+
+// copy by refernce
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+// const num = 10
+// arr[0] = 10
+
+// arr.push(200)
+
+
+// arr.push(100, 200, 300, 400, 500)
+// console.log(arr);
+
+
+const obj =
